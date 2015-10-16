ba393b2d9272dafb90f5c04477bfd496f51b17d0
peedeeX21 <peter.schrott89@gmail.com>
2015-10-16 13:13:57 +0200

    change from java.util.Date to joda.DateTime, improved CSV serialisation, added additional filters for tweets, added docs

--- src/main/scala/de/peedeex21/twittertraffic/TwitterTrafficJob.scala
+++ src/main/scala/de/peedeex21/twittertraffic/TwitterTrafficJob.scala
@@ -1,13 +1,15 @@
 package de.peedeex21.twittertraffic
 
-import java.util.Date
-
 import de.peedeex21.twittertraffic.TwitterTrafficJob.AccidentTweet
+import org.apache.commons.lang3.StringEscapeUtils
 import org.apache.flink.api.scala._
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.connectors.json.JSONParseFlatMap
 import org.apache.flink.streaming.connectors.twitter.TwitterFilterSource
 import org.apache.flink.util.Collector
+import org.apache.sling.commons.json.JSONException
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
 import play.libs.Json
 
 object TwitterTrafficJob {
@@ -15,9 +17,19 @@ object TwitterTrafficJob {
   var loginProperties: Option[String] = None
   var outputPath: Option[String] = None
 
-  case class AccidentTweet(id: Long, createdAt: Date, text: String, lon: Double, lat: Double) {
+  /**
+   * Wrapper for accident POIs.
+   *
+   * @param id tweet id
+   * @param createdAt date of creation
+   * @param text tweet text
+   * @param lon appended longitude
+   * @param lat appended latitude
+   */
+  case class AccidentTweet(id: Long, createdAt: DateTime, text: String, lang: String, lon: Double, lat: Double) {
     override def toString = {
-      this.id + "," + this.createdAt.getTime + ",\"" +  this.text + "\"," + this.lon + "," + this.lat
+      this.id + "," + this.createdAt + "," + StringEscapeUtils.escapeCsv(this.text) + "," +
+        this.lang + "," + this.lon + "," + this.lat
     }
   }
 
@@ -31,19 +43,19 @@ object TwitterTrafficJob {
     // set up the streaming execution environment
     val streamEnv = StreamExecutionEnvironment.getExecutionEnvironment
 
-    // set up the data source, filter for the term 'unfall'
+    // set up the data source, filter for the term 'accident' or 'traffic'
     val twitterSource = new TwitterFilterSource(loginProperties.get)
     twitterSource.trackTerm("accident")
     twitterSource.trackTerm("traffic")
     twitterSource.getLocations
-    //twitterSource.filterLanguage("de")
 
+    // retrieve, filter and parse tweets
     val accidentPOIs = streamEnv.addSource(twitterSource)
-      .flatMap(new AccidentTweetMapper())
+      .flatMap(new AccidentTweetParser())
 
     // emit the result
     if(outputPath.isDefined) {
-      accidentPOIs.writeAsCsv(outputPath.get)
+      accidentPOIs.writeAsText(outputPath.get)
     } else {
       accidentPOIs.print()
     }
@@ -52,13 +64,19 @@ object TwitterTrafficJob {
     streamEnv.execute("Twitter Traffic")
   }
 
+  /**
+   * Parser function for program arguments.
+   *
+   * @param args path/to/login.properties, [path/to/output]
+   * @return true on success otherwise false
+   */
   def parseArguments(args: Array[String]): Boolean = {
     if(args.length == 1) {
       this.loginProperties = Some(args(0))
       true
     } else if(args.length == 2) {
       this.loginProperties = Some(args(0))
-      this.outputPath = Some(args(1))
+      this.outputPath = Some(args(1) + "/" + new DateTime().getMillis)
       true
     } else {
       println(
@@ -72,25 +90,37 @@ object TwitterTrafficJob {
 
 }
 
-class AccidentTweetMapper extends JSONParseFlatMap[String, AccidentTweet] {
+class AccidentTweetParser extends JSONParseFlatMap[String, AccidentTweet] {
 
   // e.g. Thu Oct 15 17:10:31 +0000 2015
-  val dateFormat = new java.text.SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy")
+  val inputDatePattern = "EEE MMM dd HH:mm:ss Z yyyy"
 
   override def flatMap(in: String, out: Collector[AccidentTweet]): Unit = {
 
-    if (getString(in, "coordinates").equals("null")) {
-      // no geo location given
-      return
+    try {
+      if (getString(in, "coordinates").equals("null")) {
+        // no geo location given
+        return
+      }
+
+      if(getBoolean(in, "retweeted")) {
+        // no need for duplicates
+        return
+      }
+
+      val targetDateFormat = DateTimeFormat.forPattern(inputDatePattern).withOffsetParsed()
+
+      val id = getLong(in, "id")
+      val createdAt = targetDateFormat.parseDateTime(getString(in, "created_at"))
+      val text = getString(in, "text")
+      val lang = getString(in, "lang")
+      val coordinates = Json.parse(getString(in, "coordinates")).get("coordinates")
+
+      out.collect(new AccidentTweet(id, createdAt, text, lang,
+        coordinates.get(0).asDouble, coordinates.get(1).asDouble))
+    } catch {
+      case e: JSONException => //ignore malformed JSON
     }
-
-    val id = getLong(in, "id")
-    val createdAt = dateFormat.parse(getString(in, "created_at"))
-    val text = getString(in, "text")
-    val coordinates = Json.parse(getString(in, "coordinates")).get("coordinates")
-
-    out.collect(new AccidentTweet(id, createdAt, text,
-      coordinates.get(0).asDouble, coordinates.get(1).asDouble))
-
   }
+
 }
